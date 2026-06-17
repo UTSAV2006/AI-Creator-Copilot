3ca1114aee182665cb91f7d994832d9d6b242528
UTSAV <kutsav548@gmail.com>
2026-06-17 14:27:28 +0530

    Server.js modified

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const aiRoutes = require("./backend/routes/aiRoutes");
+
 
 app.use(express.json());
 
-app.use("/api/ai", aiRoutes);
+
 
 const PORT = 5000;
 
